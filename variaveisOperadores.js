8bd627f2913bd1c2f0c150110d4a79d0afcba2fc
Artur Vivacqua <127604196+ArturVivacqua@users.noreply.github.com>
2024-03-13 09:28:02 -0300

    Create variaveisOperadores.js

---
+++ variaveisOperadores.js
@@ -0,0 +1,8 @@
+var n1 = prompt("Digite o primeiro número")
+var n2 = prompt("Digite o segundo número")
+
+n1 = parseFloat(n1)
+n2 = parseFloat(n2)
+
+var soma igual n1 + n2
+alert("A soma dos valores é: " + soma)
